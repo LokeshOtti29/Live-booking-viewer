5945ca4ab782da831927b3e9ecf5cbc73c735cd3
LokeshOtti29 <lokeshotti456@gmail.com>
2025-07-30 21:38:17 +0530

    implemented-socket-io

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,42 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
-
+import path from "path";
+import { fileURLToPath } from "url";
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST"],
+  },
+});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = 3000;
+app.use(express.static(path.join(__dirname, "../Frontend")));
+const bookings = [];
+io.on("connection", (socket) => {
+  console.log("client connected");
+
+  bookings.forEach((booking) => {
+    socket.emit("new-booking", booking);
+  });
+  socket.on("disconnect", () => {
+    console.log("A client disconnected");
+  });
+});
+setInterval(() => {
+  const newBooking = {
+    venueName: `Venue-${Math.floor(Math.random() * 100)}`,
+    partySize: Math.floor(Math.random() * 10) + 1,
+    time: new Date().toLocaleTimeString(),
+  };
+
+  bookings.unshift(newBooking);
+  io.emit("new-booking", newBooking);
+}, 3000);
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
--- Frontend/index.js
+++ Frontend/index.js
@@ -1,8 +1,13 @@
+const socket = io("http://localhost:3000");
 let card = document.getElementsByClassName("booking-cards")[0];
 
-// let setcard = setInterval(() => {
-//   let div = document.createElement("div");
-//   div.className = "card";
-//   div.innerHTML = "<span>hello</span>";
-//   card.append(div);
-// }, 1000);
+socket.on("new-booking", (booking) => {
+  let div = document.createElement("div");
+  div.className = "card";
+  div.innerHTML = `
+    <strong>Venue:</strong> ${booking.venueName}<br>
+    <strong>Party Size:</strong> ${booking.partySize}<br>
+    <strong>Time:</strong> ${booking.time}
+  `;
+  card.prepend(div);
+});
